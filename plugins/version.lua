440f3ecd2b9f5882c7694a63af495196f0a37e94
Shahab <safaeishahab@yahoo.com>
2016-03-02 20:49:01 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,8 +14,9 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
+  return 'E M C Team Bot V1.0 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/EMCTEAM-IRAN/EMC\n\nDeveloper
+:\n@Shahabambesik\n'
+  --[[ Checkout https://github.com/EMCTEAM-IRAN/EMC Special thanks to Yago Prez and josep
   --GNU GPL v2 license.]]
 end
 
