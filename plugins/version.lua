70437bd172144d10df7f7cb772142912323b5926
Shahab <safaeishahab@yahoo.com>
2016-03-02 21:09:43 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,22 +1,10 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
 
 do
 
 function run(msg, matches)
   return 'E M C Team Bot V1.0 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/EMCTEAM-IRAN/EMC\n\nDeveloper
 :\n@Shahabambesik\n'
-  --[[ Checkout https://github.com/EMCTEAM-IRAN/EMC Special thanks to Yago Prez and josep
+  --[[ Checkout https://github.com/EMCTEAM-IRAN/EMCBot Special thanks to Yago Prez and josep
   --GNU GPL v2 license.]]
 end
 
