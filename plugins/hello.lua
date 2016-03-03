deed1ae3dc3d7f95f4c0419570ba97ce2aef8852
Shahab <safaeishahab@yahoo.com>
2016-03-04 00:21:33 +0330

    Create hello.lua

---
+++ plugins/hello.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+  return "Hello, " .. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "say hello to [name]",
+  patterns = {
+    "^say hello to (.*)$",
+    "^Say hello to (.*)$",
+    "^#Say hello to (.*)$",
+
+    "^#say hello to (.*)$"
+  }, 
+  run = run 
+}
+
+end
